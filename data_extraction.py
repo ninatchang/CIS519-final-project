68bee722c40f947917862d9e10d5365522995db5
ninatchang <ninatchang@berkeley.edu>
2018-12-06 13:18:53 -0500

    fixed a bug in data extraction + changed data repr

--- data_extraction.py
+++ data_extraction.py
@@ -11,20 +11,22 @@ def parse_amazon_data(file_path):
 
 	Output:
 		a tuple consisting of 
-			1. a list of reviews (a big blob of string)
+			1. a list of reviews (a list of strings)
 			2. a list of good/bad labels (stored as ints. 0 = bad, 1 = good)
 	'''
 	review_str_list = []
 	labels_list = []
 
 	with open(file_path, 'r') as f:
+		words = []
 		for line in f:
 			label_str, review_str = line.split(' ', 1)
 			label = int(label_str[-1]) - 1		# original data is labeled with 1 and 2. change to labels of 0 and 1
+			words = review_str.split()			# split on spaces
 		  
 			# Not too sure what the best DS is here but gonna store everything in list for now
-			review_str_list.append(review_str)
-			labels_list.append(labels_list)
+			review_str_list.append(words)
+			labels_list.append(label)
 
 	return (review_str_list, labels_list)
 
@@ -39,27 +41,28 @@ def parse_imdb_data(file_path):
 	
 	Output:
 		a tuple consisting of
-			1. a list of reviews (a big blob of string)
+			1. a list of reviews (a list of strings)
 			2. a list of good/bad labels (stored as ints. 0 = bad, 1 = good)
 	'''
 	review_str_list = []
 	labels_list = []
 
 	for folder_name in os.listdir(file_path):
+		print(folder_name)
 		if folder_name == "pos":
 			label = 1
 		elif folder_name == "neg":
 			label = 0
 		else:
 			# We are not interested in these data!!
-			break
+			continue
 
 		for file_name in os.listdir(file_path + "/" + folder_name):
 			with open(file_path + "/" + folder_name + "/" + file_name, 'r', errors='ignore') as f:
-				review_str = ""
+				words = []
 				for line in f:
-					review_str += line
-			review_str_list.append(review_str)
+					words += line.split()
+			review_str_list.append(words)
 			labels_list.append(label)
 
 	return (review_str_list, labels_list)
